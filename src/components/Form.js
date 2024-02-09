501bc7775b9020b43cc4ca0183eb5ee36f8ee832
Dakota Vikdal <dakota.james19@hotmail.com>
2024-02-09 18:05:21 -0500

    form submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,63 @@
 import React, { useState } from "react";
 
-function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+function Form () {
+  const [firstName, setFirstName] = useState("Bill")
+  const [lastName, setLastName] = useState("Julien")
+  const [submittedData, setSubmitted] = useState([])
+  const [errors, setErrors] = useState([])
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  function handleFirstName(e) {
+    setFirstName(e.target.value)
   }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  function handleLastName(e) {
+    setLastName(e.target.value)
   }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if(firstName){
+      console.log(firstName)
+      const formData= { firstName: firstName, lastName: lastName};
+      const dataArray = [...submittedData, formData]
+      setSubmitted(dataArray)
+      setFirstName("");
+      setLastName("");
+      setErrors([])
+    }
+   else {
+    setErrors(["You need to input a name!"])
+    }
+  }
+
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    )
+  })
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange= {handleFirstName} value={firstName}/>
+        <input type="text" onChange= {handleLastName} value= {lastName}/>
+        <button type="submit">Submit</button>
+      </form>
+      {errors.length  > 0 
+        ? errors.map((error, index) => (
+          <p key={index} style={{ color: "red"}}>
+            {error}
+          </p>
+        ))
+          : null}
+
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
+  )
+
 }
 
-export default Form;
+export default Form
